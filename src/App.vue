ed6809137d8c0b8cae08d3d2ebfaf09340b27abf
Gerald Murphy <geramurp52@gmail.com>
2025-12-02 18:02:40 -0700

    Add routing links to the nav and hook up routes

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,20 @@
     <nav>
       <ul>
         <li class="nav-item">
-          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
-          Build-a-Bot
+          <router-link :to="{ name: 'Home' }" class="nav-link" active-class="home-active">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />
+            Build-a-Bot
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Build' }" class="nav-link">
+            Build
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Search' }" class="nav-link">
+            Search
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -49,6 +61,11 @@ ul {
   display: flex;;
 }
 
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+
 .nav-item {
   display: inline-block;
   padding: 5px 10px;
@@ -59,5 +76,14 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+
+.active-link {
+  color: white;
+}
+
+.home-active {
+  color: white;
+  text-shadow: 2px 2px black;
 }
 </style>
